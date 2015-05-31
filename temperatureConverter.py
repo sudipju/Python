d5fed563a811d5ba1c0827e92321df8925cc70be
sudip kumar das <sudipku@localhost.localdomain>
2015-05-31 17:26:28 +0200

    Initial version of temperature converter

---
+++ temperatureConverter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import sys
+
+def _typeCheker(param):
+    if (type(param) == int) :
+        param=float(param)
+    elif (type(param) == float) :
+        param=param
+    else:
+        errorType=type(param).__name__
+        raise TypeError(str(errorType))
+    return param
+
+def centrigradeToFahrenheit(centigradeVal):
+    Tc = _typeCheker(centigradeVal)
+    Tf = (9/5)*Tc + 32
+    return Tf
+
+def fahrenheitToCentrigrade(fahrenheit):
+    Tf = _typeCheker(fahrenheit)
+    Tc = (5/9)*(Tf - 32)
+    return Tc
+
+if __name__=='__main__':
+    inVal = sys.argv[1]
+    #intParam = float(inVal)
+    intParam = inVal
+    try:
+        fahrenheit = centrigradeToFahrenheit(intParam)
+        print("Fahrenheit = ",fahrenheit)
+        centrigrade=fahrenheitToCentrigrade(fahrenheit)
+        print("centrigrade = ",centrigrade)
+    except TypeError as err:
+        #print("err= ", err)
+        print("Type given is = ", str(err) + " but accepted types are int and float !")
+        quit()
+    
+    
+
